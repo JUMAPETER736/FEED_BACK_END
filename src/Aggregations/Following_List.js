92ff558cf5acb24200d51ab5b9f408ac7af8e700
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-10 10:46:12 +0200

    In the Aggregations Package creating the Following_List

---
+++ src/Aggregations/Following_List.js
@@ -0,0 +1,43 @@
+const unifiedFollowingListAggregation = () => {
+    return [
+        {
+      $lookup: {
+        // lookup for the each user's profile
+        from: "socialprofiles",
+        localField: "_id",
+        foreignField: "owner",
+        as: "profile",
+        pipeline: [
+          {
+            $project: {
+              firstName: 1,
+              lastName: 1,
+              bio: 1,
+              location: 1,
+              countryCode: 1,
+              phoneNumber: 1,
+              coverImage: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: { profile: { $first: "$profile" } },
+    },
+    {
+      $project: {
+        username: 1,
+        email: 1,
+        isEmailVerified: 1,
+        avatar: 1,
+        profile: 1,
+      },
+    },
+    ];
+};
+
+
+export {
+    unifiedFollowingListAggregation
+}
