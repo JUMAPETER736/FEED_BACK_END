ccb1799ed6f8052c7bc6ce4651b18ed3009fc268
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-11 10:43:07 +0200

    Creation of the Feed Package, deleteCommentReply

--- src/Controllers/OverView/Feed/Comments_Reply_Controller.js
+++ src/Controllers/OverView/Feed/Comments_Reply_Controller.js
@@ -264,3 +264,126 @@ const addCommentReply = asyncHandler(async (req, res) => {
 
 
 
+const getCommentsReply = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { page = 1, limit = 5 } = req.query;
+  const commentAggregation = FeedCommentReply.aggregate([
+    {
+      $match: {
+        commentId: new mongoose.Types.ObjectId(commentId),
+      },
+    },
+    {
+      $sort: { createdAt: -1 }, // Sort by createdAt in descending order
+    },
+    {
+      $lookup: {
+        from: "feedlikes",
+        localField: "_id",
+        foreignField: "commentReplyId",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "feedlikes",
+        localField: "_id",
+        foreignField: "commentReplyId",
+        as: "isLiked",
+        pipeline: [
+          {
+            $match: {
+              likedBy: new mongoose.Types.ObjectId(req.user?._id),
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $lookup: {
+        from: "socialprofiles",
+        localField: "author",
+        foreignField: "owner",
+        as: "author",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "account",
+              pipeline: [
+                {
+                  $project: {
+                    avatar: 1,
+                    email: 1,
+                    username: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $project: {
+              firstName: 1,
+              lastName: 1,
+              account: 1,
+            },
+          },
+          {
+            $addFields: {
+              account: { $first: "$account" },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        author: { $first: "$author" },
+        likes: { $size: "$likes" },
+        isLiked: {
+          $cond: {
+            if: {
+              $gte: [
+                {
+                  // if the isLiked key has document in it
+                  $size: "$isLiked",
+                },
+                1,
+              ],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+  ]);
+
+  const comments = await FeedCommentReply.aggregatePaginate(
+    commentAggregation,
+    getMongoosePaginationOptions({
+      page,
+      limit,
+      customLabels: {
+        totalDocs: "totalReplyComments",
+        docs: "comments",
+      },
+    })
+  );
+
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        comments,
+        "feed Comment replies fetched successfully"
+      )
+    );
+});
+
+
