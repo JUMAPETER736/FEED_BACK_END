e223b63e21eaf988dc6fd4a74b89d234b9e40a04
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-21 12:22:00 +0200

    feat: add likeNotificationAggregation pipeline utility
    - lookup sender user data from users collection
    - project username, avatar, email fields only
    - flatten sender array to single object with arrayElemAt

--- src/Controllers/OverView/Notifications/Notifications_Controller.js
+++ src/Controllers/OverView/Notifications/Notifications_Controller.js
@@ -104,3 +104,30 @@ const notificationCommonAggregation = (receiverId) => {
     },
   ];
 };
+
+const likeNotificationAggregation = () => {
+  return [
+    {
+      $lookup: {
+        from: 'users',
+        foreignField: '_id',
+        localField: 'sender',
+        as: 'sender',
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+              email: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        sender: { $arrayElemAt: ['$sender', 0] }, // Take the first element of the array as sender
+      },
+    },
+  ];
+};
