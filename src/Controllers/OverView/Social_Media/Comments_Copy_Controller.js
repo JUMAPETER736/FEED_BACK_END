5ce3db75bf050bbb37620dab54efa31c34a5d0ce
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-21 18:56:00 +0200

    Adding postCommonAggregation helper, lookup socialcomments sociallikes socialbookmarks and socialprofiles, compute likes comments counts with isLiked and isBookmarked flags & merge author account details with authorId field

---
+++ src/Controllers/OverView/Social_Media/Comments_Copy_Controller.js
@@ -0,0 +1,145 @@
+
+
+import mongoose from "mongoose";
+import { SocialComment } from "../../../models/apps/social-media/comment.models.js";
+import { ApiResponse } from "../../../utils/ApiResponse.js";
+import { asyncHandler } from "../../../utils/asyncHandler.js";
+import { getMongoosePaginationOptions } from "../../../utils/helpers.js";
+import { ApiError } from "../../../utils/ApiError.js";
+import {User} from "../../../models/apps/auth/user.models.js";
+
+import { emitSocketEvent } from "../../../socket/index.js";
+// import CommentNotification from "../../../models/apps/notifications/commentNotification.model.js"
+/**
+ * @param {string} userId
+ * @param {import("express").Request} req
+ * @description Utility function which returns the pipeline stages to structure the social post schema with calculations like, likes count, comments count, isLiked, isBookmarked etc
+ * @returns {mongoose.PipelineStage[]}
+ */
+////new code here 
+const postCommonAggregation = (req) => {
+  return [
+    {
+      $lookup: {
+        from: "socialcomments",
+        localField: "_id",
+        foreignField: "postId",
+        as: "comments",
+      },
+    },
+    {
+      $lookup: {
+        from: "sociallikes",
+        localField: "_id",
+        foreignField: "postId",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "sociallikes",
+        localField: "_id",
+        foreignField: "postId",
+        as: "isLiked",
+        pipeline: [
+          {
+            $match: {
+              likedBy: new mongoose.Types.ObjectId(req.user?._id),
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "socialbookmarks",
+        localField: "_id",
+        foreignField: "postId",
+        as: "isBookmarked",
+        pipeline: [
+          {
+            $match: {
+              bookmarkedBy: new mongoose.Types.ObjectId(req.user?._id),
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "socialprofiles",
+        localField: "author",
+        foreignField: "owner",
+        as: "author",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "account",
+              pipeline: [
+                {
+                  $project: {
+                    avatar: 1,
+                    email: 1,
+                    username: 1,
+                    _id: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              account: { $first: "$account" },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        // author: { $first: "$author" },
+        author: {
+          $mergeObjects: [
+            { $first: "$author" },
+            { authorId: "$author._id" }, // Assuming the author ID is available in the "author" field
+          ],
+        },
+        likes: { $size: "$likes" },
+        comments: { $size: "$comments" },
+        isLiked: {
+          $cond: {
+            if: {
+              $gte: [
+                {
+                  // if the isLiked key has document in it
+                  $size: "$isLiked",
+                },
+                1,
+              ],
+            },
+            then: true,
+            else: false,
+          },
+        },
+        isBookmarked: {
+          $cond: {
+            if: {
+              $gte: [
+                {
+                  // if the isBookmarked key has document in it
+                  $size: "$isBookmarked",
+                },
+                1,
+              ],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+  ];
+};
