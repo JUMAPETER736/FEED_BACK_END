c697e1401e1c9b1e3ef1ea327194c6a8fe9a4eb6
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-21 19:50:00 +0200

    Adding deleteComment controller, find and delete SocialComment scoped to author by commentId, throw 404 if comment not found or user is unauthorized & return deleted comment in response

--- src/Controllers/OverView/Social_Media/Comments_Copy_Controller.js
+++ src/Controllers/OverView/Social_Media/Comments_Copy_Controller.js
@@ -413,3 +413,26 @@ const getPostComments = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, comments, "Post comments fetched successfully"));
 });
+
+
+
+const deleteComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const deletedComment = await SocialComment.findOneAndDelete({
+    _id: new mongoose.Types.ObjectId(commentId),
+    author: req.user?._id,
+  });
+
+  if (!deletedComment) {
+    throw new ApiError(
+      404,
+      "Comment is already deleted or you are not authorized for this action."
+    );
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { deletedComment }, "Comment deleted successfully")
+    );
+});
