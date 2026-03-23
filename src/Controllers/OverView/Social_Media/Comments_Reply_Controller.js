2295719827475c77c58c9c87d6a4861c4cab81e7
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-23 11:24:00 +0200

    Adding unifiedNotificationCommonAggregation helper to comment reply controller, lookup users collection by sender field projecting username avatar and email & addFields sender as arrayElemAt index zero for consistent single sender object shape

--- src/Controllers/OverView/Social_Media/Comments_Reply_Controller.js
+++ src/Controllers/OverView/Social_Media/Comments_Reply_Controller.js
@@ -163,3 +163,31 @@ const postCommonAggregation = (req) => {
     },
   ];
 };
+
+
+const unifiedNotificationCommonAggregation = () => {
+  return [
+    {
+      $lookup: {
+        from: "users",
+        foreignField: "_id",
+        localField: "sender",
+        as: "sender",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+              email: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        sender: { $arrayElemAt: ["$sender", 0] }, // Take the first element of the array as sender
+      },
+    },
+  ];
+};
