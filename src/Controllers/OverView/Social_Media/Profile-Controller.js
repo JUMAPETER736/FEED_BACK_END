576e0af1787b1f8a3fb8d4160623bba185538540
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-23 14:24:00 +0200

    Adding getUserSocialProfile utility function, aggregate SocialProfile by owner with lookup to users projecting avatar username email and isEmailVerified, lookup socialfollows for following and followedBy arrays, compute followersCount and followingCount in addFields then project out raw arrays & check SocialFollow for isFollowing flag when logged in user differs from profile owner and return merged userProfile with isFollowing

---
+++ src/Controllers/OverView/Social_Media/Profile-Controller.js
@@ -0,0 +1,102 @@
+
+
+import mongoose from "mongoose";
+import { User } from "../../../models/apps/auth/user.models.js";
+import { SocialFollow } from "../../../models/apps/social-media/follow.models.js";
+import { SocialProfile } from "../../../models/apps/social-media/profile.models.js";
+import { ApiError } from "../../../utils/ApiError.js";
+import { ApiResponse } from "../../../utils/ApiResponse.js";
+import { asyncHandler } from "../../../utils/asyncHandler.js";
+import {
+  getLocalPath,
+  getStaticFilePath,
+  removeLocalFile,
+} from "../../../utils/helpers.js";
+
+/**
+ *
+ * @param {string} userId
+ * @param {import("express").Request} req
+ * @description A utility function, which querys the {@link SocialProfile} model and returns the profile with account details
+ */
+const getUserSocialProfile = async (userId, req) => {
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  let profile = await SocialProfile.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "account",
+        pipeline: [
+          {
+            $project: {
+              avatar: 1,
+              username: 1,
+              email: 1,
+              isEmailVerified: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "socialfollows",
+        localField: "owner",
+        foreignField: "followerId",
+        as: "following", // users that are followed by current user
+      },
+    },
+    {
+      $lookup: {
+        from: "socialfollows",
+        localField: "owner",
+        foreignField: "followeeId",
+        as: "followedBy", // users that are following the current user
+      },
+    },
+    {
+      $addFields: {
+        account: { $first: "$account" },
+        followersCount: { $size: "$followedBy" },
+        followingCount: { $size: "$following" },
+      },
+    },
+    {
+      $project: {
+        followedBy: 0,
+        following: 0,
+      },
+    },
+  ]);
+
+  let isFollowing = false;
+
+  if (req.user?._id && req.user?._id?.toString() !== userId.toString()) {
+    // Check if there is a logged in user and logged in user is NOT same as the profile that is being loaded
+    // In such case we will check if the logged in user follows the loaded profile user
+    const followInstance = await SocialFollow.findOne({
+      followerId: req.user?._id, // logged in user. If this is null `isFollowing` will be false
+      followeeId: userId,
+    });
+    isFollowing = followInstance ? true : false;
+  }
+
+  const userProfile = profile[0];
+
+  if (!userProfile) {
+    throw new ApiError(404, "User profile does not exist");
+  }
+  return { ...userProfile, isFollowing };
+};
