65827283498e6e76dac7199486678555c5d2120c
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-25 10:04:17 +0200

    Creating the Show More option, making the populateUserDetails

--- src/Controllers/OverView/Social_Media/Show_More_Option_Controller.js
+++ src/Controllers/OverView/Social_Media/Show_More_Option_Controller.js
@@ -21,3 +21,41 @@ const validateUserId = (userId) => {
     throw new ApiError(400, "Invalid user ID");
   }
 };
+
+
+const populateUserDetails = async (userId) => {
+  const user = await User.findById(userId);
+  if (!user) return null;
+
+  const socialProfile = await SocialProfile.findOne({ owner: userId });
+
+  const userDetails = {
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  };
+
+  if (user.avatar) {
+    userDetails.avatar = {
+      url: user.avatar.url,
+      localPath: user.avatar.localPath,
+      _id: user.avatar._id,
+    };
+  }
+
+  if (socialProfile) {
+    userDetails.coverImage = socialProfile.coverImage;
+    userDetails.firstName = socialProfile.firstName;
+    userDetails.lastName = socialProfile.lastName;
+    userDetails.bio = socialProfile.bio;
+    userDetails.dob = socialProfile.dob;
+    userDetails.location = socialProfile.location;
+    userDetails.countryCode = socialProfile.countryCode;
+    userDetails.phoneNumber = socialProfile.phoneNumber;
+    userDetails.profileId = socialProfile._id;
+  }
+
+  return userDetails;
+};
