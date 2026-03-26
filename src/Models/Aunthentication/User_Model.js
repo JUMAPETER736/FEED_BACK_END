b5c6a039035a9ed29d28b91b8c9e20aa90f99589
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-26 11:59:06 +0200

    In the Package of the Aunthentication, Still with the Aunthentication Model

--- src/Models/Aunthentication/User_Model.js
+++ src/Models/Aunthentication/User_Model.js
@@ -164,3 +164,82 @@ userSchema.post("save", async function (user, next) {
   }
   next();
 });
+
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      role: this.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+/**
+ * @description Method responsible for generating tokens for email verification, password reset etc.
+ */
+
+
+userSchema.methods.generateTemporaryToken = function () {
+  // This token should be client facing
+  // for example: for email verification unHashedToken should go into the user's mail
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+
+  // This should stay in the DB to compare at the time of verification
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+  // This is the expiry time for the token (20 minutes)
+  const tokenExpiry = Date.now() + USER_TEMPORARY_TOKEN_EXPIRY;
+
+  return { unHashedToken, hashedToken, tokenExpiry };
+};
+
+// OTP-BASED PASSWORD RESET METHODS
+userSchema.methods.generateResetToken = function () {
+  const resetToken = Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+
+  this.resetPasswordToken = resetToken;
+  this.resetPasswordExpire = Date.now() + 3600000; // 1 hour
+
+  return resetToken;
+};
+
+// Corrected OTP generation formula
+userSchema.methods.generateResetOTP = function () {
+
+  //  Generate proper 6-digit OTP
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+
+  this.resetOTP = otp;
+  this.resetOTPExpire = Date.now() + 600000; // 10 minutes
+
+  console.log("Generated OTP:", otp, "Length:", otp.length);
+
+  return otp;
+};
+
+export const User = mongoose.model("User", userSchema);
+
+
