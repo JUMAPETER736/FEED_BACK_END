bc8f0d6b595950523a9e7ac3ec8d6f74238cb9da
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-27 09:09:16 +0200

    Creating the Business Package, Business Comments Rreply Model

---
+++ src/Models/Business/Business_Comment_Reply_Model.js
@@ -0,0 +1,94 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      // required: true,
+    },
+    contentType: {
+      type: String,
+      // required: true,
+    },
+    commentId: {
+      type: Schema.Types.ObjectId,
+      ref: "BusinessComment",
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    duration: {
+      type: String,
+    },
+    fileType: {
+      type: String,
+    },
+    fileName: {
+      type: String,
+    },
+    fileSize: {
+      type: String,
+    },
+    numberOfPages: {
+      type: String,
+    },
+    audios: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    thumbnail: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    images: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    videos: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    docs: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    gifs: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const BusinessCommentReply = mongoose.model(
+  "BusinessCommentReply",
+  commentSchema
+);
