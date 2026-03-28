d5ba79ad05301eca258b3f381fda29d923818d7a
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-28 16:37:40 +0200

    In the Shorts package creating the Comment Reply Model

---
+++ src/Models/Shorts/Comment_Reply_Model.js
@@ -0,0 +1,96 @@
+import mongoose, { Schema } from "mongoose";
+import { User } from "../auth/user.models.js";
+import { SocialPost } from "./post.models.js";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      // required: true,
+    },
+    contentType: {
+      type: String,
+      // required: true,
+    },
+    commentId: {
+      type: Schema.Types.ObjectId,
+      ref: "SocialComment",
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    duration: {
+      type: String,
+    },
+    fileType: {
+      type: String,
+    },
+    fileName: {
+      type: String,
+    },
+    fileSize: {
+      type: String,
+    },
+    numberOfPages: {
+      type: String,
+    },
+    audios: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    thumbnail: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    images: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    videos: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    docs: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
+    gifs: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const SocialCommentReply = mongoose.model(
+  "SocialCommentReply",
+  commentSchema
+);
