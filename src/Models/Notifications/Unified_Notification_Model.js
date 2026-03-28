ce554613b446a5355d9f634090fec1ed83cee48e
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-28 15:09:18 +0200

    In the Notofication creating the Unified Notification Model

---
+++ src/Models/Notifications/Unified_Notification_Model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const unifiedNotificationSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    message: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String, // E.g., "reply", "comment", "like", "follow", "system", etc.
+      required: true,
+    },
+    data: {
+      type: Schema.Types.Mixed, // Stores type-specific data
+    },
+  },
+  { timestamps: true }
+);
+
+const UnifiedNotification = mongoose.model("UnifiedNotification", unifiedNotificationSchema);
+
+export default UnifiedNotification;
