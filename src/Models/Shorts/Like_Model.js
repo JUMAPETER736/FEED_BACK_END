2ad45f00633e5d2b8a0d9c0ee85b299075243364
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-28 16:58:19 +0200

    In the Shorts package creating the Like Model

---
+++ src/Models/Shorts/Like_Model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+import { User } from "../auth/user.models.js";
+import { SocialPost } from "./post.models.js";
+import { SocialComment } from "./comment.models.js";
+
+
+const likeSchema = new Schema(
+  {
+    postId: {
+      type: Schema.Types.ObjectId,
+      ref: "SocialPost",
+      default: null,
+    },
+    commentId: {
+      type: Schema.Types.ObjectId,
+      ref: "SocialComment",
+      default: null,
+    },
+    commentReplyId: {
+      type: Schema.Types.ObjectId,
+      ref: "SocialCommentReply",
+      default: null,
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const SocialLike = mongoose.model("SocialLike", likeSchema);
