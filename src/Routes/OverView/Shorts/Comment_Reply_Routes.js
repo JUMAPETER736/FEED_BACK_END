e33ab2cc4f372e6f802001e7d2d0e935b0937f70
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-29 17:43:25 +0200

    In the Routes of overview, Shorts package creating the Comments Reply Routes

---
+++ src/Routes/OverView/Shorts/Comment_Reply_Routes.js
@@ -0,0 +1,59 @@
+import { Router } from "express";
+import {
+  addCommentReply,
+  getCommentsReply,
+  deleteCommentReply,
+  updateCommentReply,
+} from "../../../controllers/apps/social-media/comment.reply.controllers.js";
+import {
+  getLoggedInUserOrIgnore,
+  verifyJWT,
+} from "../../../middlewares/auth.middlewares.js";
+import { commentContentValidator } from "../../../validators/apps/social-media/comment.validators.js";
+import { validate } from "../../../validators/validate.js";
+import { mongoIdPathVariableValidator } from "../../../validators/common/mongodb.validators.js";
+import { upload } from "../../../middlewares/commentsmulter.middlewares.js";
+import { MAXIMUM_SOCIAL_POST_IMAGE_COUNT } from "../../../constants.js";
+const router = Router();
+
+router
+  .route("/comment/:commentId")
+  .get(
+    getLoggedInUserOrIgnore,
+    mongoIdPathVariableValidator("commentId"),
+    validate,
+    getCommentsReply
+  )
+  .post(
+    verifyJWT,
+    mongoIdPathVariableValidator("commentId"),
+    upload.fields([
+      { name: "image", maxCount: MAXIMUM_SOCIAL_POST_IMAGE_COUNT },
+      { name: "thumbnail", maxCount: MAXIMUM_SOCIAL_POST_IMAGE_COUNT },
+      { name: "video", maxCount: MAXIMUM_SOCIAL_POST_IMAGE_COUNT },
+      { name: "audio", maxCount: MAXIMUM_SOCIAL_POST_IMAGE_COUNT },
+      // { name: "gif", maxCount: MAXIMUM_SOCIAL_POST_IMAGE_COUNT },
+      { name: "docs", maxCount: MAXIMUM_SOCIAL_POST_IMAGE_COUNT },
+    ]),
+    // commentContentValidator(),
+    // validate,
+    addCommentReply
+  );
+
+router
+  .route("/:commentReplyId")
+  .delete(
+    verifyJWT,
+    mongoIdPathVariableValidator("commentReplyId"),
+    validate,
+    deleteCommentReply
+  )
+  .patch(
+    verifyJWT,
+    mongoIdPathVariableValidator("commentReplyId"),
+    commentContentValidator(),
+    validate,
+    updateCommentReply
+  );
+
+export default router;
