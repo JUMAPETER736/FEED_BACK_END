32c9ff3161b144c2d350fe024a6a18ff5ffb5a2a
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-30 14:22:02 +0200

    Creating the Data, Model and Data Model in it

---
+++ src/Data/Others/Data.js
@@ -0,0 +1,110 @@
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "Uy Scuti",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Uy Scuti",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      name: "Uy Scuti",
+      email: "uyscuti@gmail.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Uy Scuti",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+      {
+        name: "Uy Scuti",
+        email: "uyscuti@gmail.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Uy Scuti",
+    groupAdmin: {
+      name: "Uy Scuti",
+      email: "uyscuti@gmail.com",
+    },
+  },
+];
+
+module.exports={chats}
