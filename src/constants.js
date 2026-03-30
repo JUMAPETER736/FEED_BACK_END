66cce6f3663bfbccecfa98e602ceceefc6112f96
JUMAOPPETER736 <jumapeter736@gmail.com>
2026-03-30 12:37:45 +0200

    Creating the Constants JS

---
+++ src/constants.js
@@ -0,0 +1,123 @@
+/**
+ * @type {{ ADMIN: "ADMIN"; USER: "USER"} as const}
+ */
+export const UserRolesEnum = {
+  ADMIN: "ADMIN",
+  USER: "USER",
+};
+
+export const AvailableUserRoles = Object.values(UserRolesEnum);
+
+/**
+ * @type {{ PENDING: "PENDING"; CANCELLED: "CANCELLED"; DELIVERED: "DELIVERED" } as const}
+ */
+export const OrderStatusEnum = {
+  PENDING: "PENDING",
+  CANCELLED: "CANCELLED",
+  DELIVERED: "DELIVERED",
+};
+
+export const AvailableOrderStatuses = Object.values(OrderStatusEnum);
+
+/**
+ * @type {{ UNKNOWN:"UNKNOWN"; RAZORPAY: "RAZORPAY"; PAYPAL: "PAYPAL"; } as const}
+ */
+export const PaymentProviderEnum = {
+  UNKNOWN: "UNKNOWN",
+  RAZORPAY: "RAZORPAY",
+  PAYPAL: "PAYPAL",
+};
+
+export const AvailablePaymentProviders = Object.values(PaymentProviderEnum);
+
+/**
+ * @type {{ FLAT:"FLAT"; } as const}
+ */
+export const CouponTypeEnum = {
+  FLAT: "FLAT",
+  // PERCENTAGE: "PERCENTAGE",
+};
+
+export const AvailableCouponTypes = Object.values(CouponTypeEnum);
+
+/**
+ * @type {{ GOOGLE: "GOOGLE"; GITHUB: "GITHUB"; EMAIL_PASSWORD: "EMAIL_PASSWORD"} as const}
+ */
+export const UserLoginType = {
+  GOOGLE: "GOOGLE",
+  GITHUB: "GITHUB",
+  EMAIL_PASSWORD: "EMAIL_PASSWORD",
+};
+
+export const AvailableSocialLogins = Object.values(UserLoginType);
+
+/**
+ * @type {{ MOST_VIEWED: "mostViewed"; MOST_LIKED: "mostLiked"; LATEST: "latest"; OLDEST: "oldest"} as const}
+ */
+export const YouTubeFilterEnum = {
+  MOST_VIEWED: "mostViewed",
+  MOST_LIKED: "mostLiked",
+  LATEST: "latest",
+  OLDEST: "oldest",
+};
+
+export const AvailableYouTubeFilters = Object.values(YouTubeFilterEnum);
+
+export const USER_TEMPORARY_TOKEN_EXPIRY = 20 * 60 * 1000; // 20 minutes
+
+export const MAXIMUM_SUB_IMAGE_COUNT = 8;
+export const MAXIMUM_SOCIAL_POST_IMAGE_COUNT = 10;
+
+export const DB_NAME = "flashapi";
+
+export const paypalBaseUrl = {
+  sandbox: "https://api-m.sandbox.paypal.com",
+};
+
+export const NotificationEventEnum = {
+  NEW_NOTIFICATION: "newNotification",
+  UNREAD_COUNT_UPDATE: "unreadCountUpdate",
+  NOTIFICATION_READ: "notificationRead",
+  ALL_NOTIFICATIONS_READ: "allNotificationsRead",
+  // Other notification events can be added here
+};
+
+/**
+ * @description set of events that we are using in chat app. more to be added as we develop the chat app
+ */
+export const ChatEventEnum = Object.freeze({
+  // ? once user is ready to go
+  CONNECTED_EVENT: "connected",
+  // ? when user gets disconnected
+  DISCONNECT_EVENT: "disconnect",
+  // ? when user joins a socket room
+  JOIN_CHAT_EVENT: "joinChat",
+  // ? when participant gets removed from group, chat gets deleted or leaves a group
+  LEAVE_CHAT_EVENT: "leaveChat",
+  // ? when admin updates a group name
+  UPDATE_GROUP_NAME_EVENT: "updateGroupName",
+  // ? when new message is received
+  MESSAGE_RECEIVED_EVENT: "messageReceived",
+  // ? when there is new one on one chat, new group chat or user gets added in the group
+  NEW_CHAT_EVENT: "newChat",
+  // ? when there is an error in socket
+  SOCKET_ERROR_EVENT: "socketError",
+  // ? when participant stops typing
+  STOP_TYPING_EVENT: "stopTyping",
+  // ? when participant starts typing
+  TYPING_EVENT: "typing",
+  // ? message sent
+  MESSAGE_SENT_EVENT: "messageSent",
+  // ? when receiver receives the message
+  MESSAGE_DELIVERED_EVENT: "messageDelivered",
+  //? when the receivers opens the message
+  MESSAGE_SEEN_EVENT: "messageSeen",
+  // ? when participant's last seen information is updated
+  LAST_SEEN_EVENT: "lastSeen",
+  //? when a participant in a group chat receives the message
+  ACKNOWLEDGE_MESSAGE_EVENT: "messageAck",
+  //? when a receiver downloads a message attachment
+  ATTACHMENT_DOWNLOADED_EVENT: "downloaded",
+});
+
+export const AvailableChatEvents = Object.values(ChatEventEnum);
